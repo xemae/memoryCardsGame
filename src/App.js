e91ae7ca70a1cc04c73aecffdaff6b4ebdfa6d67
alina <alina@alinas-MacBook-Pro.local>
2023-02-18 21:04:42 +0400

    need to add cless to card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
+import React from "react";
+import {useDispatch, useSelector} from "react-redux";
 import './App.css';
 import s from './app.module.css'
+import {start} from "./redux-toolkit/toolkitSlice";
 
 function App() {
+    const cards = useSelector(state => state.toolkit.cards)
+    const dispatch = useDispatch()
 
   return (
     <div className="App">
@@ -10,7 +14,7 @@ function App() {
         <div className={s.container}>
           <div className={s.timer}></div>
           <div className={s.cards}></div>
-          <button className={s.start}>start</button>
+          <button className={s.start} onClick={() => dispatch(start)}>start</button>
 
         </div>
       </header>
