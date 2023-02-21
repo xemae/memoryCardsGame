97f7be8d0b5e129aff3ea1ea19cfcaba9a2317cf
alina <alina@alinas-MacBook-Pro.local>
2023-02-22 03:07:19 +0400

    card cover

--- src/Cover.js
+++ src/Cover.js
@@ -1,23 +1,27 @@
 import React, {useState} from "react";
 import s from "./app.module.css";
 import {useSelector} from "react-redux";
-// eslint-disable-next-line react-hooks/rules-of-hooks
 
 const Cover = () => {
-    const [coverShowed, setCoverShowed] = useState(true)
+    // const [coverShowed, setCoverShowed] = useState(true)
     const cards = useSelector(state => state.toolkit.cards)
-    cards.map(i => {
+    const covers = cards.map(i => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const [coverShowed, setCoverShowed] = useState(true)
+
         return <div
             className={s.cover}
-            key={i.toString()}
-            id={Math.random().toString()}
-            // onClick={() => this.classList.add('s.cardCover')}
+            key={Math.random()}
+            // id={Math.random().toString()}
+            style={{backgroundColor: coverShowed ? 'white' : '#af3f3f'}}
             onClick={() => setCoverShowed(false)}
-            // style={{backgroundColor: coverShowed ? 'white' : 'none'}}
         >
-            <div style={{backgroundColor: coverShowed ? 'white' : 'none'}}></div>
+            {/*<div onClick={() => setCoverShowed(false)}*/}
+            {/*     className={s.cover}*/}
+            {/*     style={{backgroundColor: coverShowed ? 'white' : '#af3f3f'}}></div>*/}
         </div>
     })
+    return covers
 }
 
 export default Cover
